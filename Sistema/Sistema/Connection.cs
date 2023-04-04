763f322235a7708c773e37cfe1b69ac4c28de2ee
David Elizondo <61263018+Darent28@users.noreply.github.com>
2023-04-04 17:25:59 -0600

    Sistema.Datos

---
+++ Sistema/Sistema/Connection.cs
@@ -0,0 +1,62 @@
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sistema.Datos
+{
+    public class Connection
+    {
+        private string Base;
+        private string Servidor;
+        private string Usuario;
+        private string Clave;
+        private bool Seguridad;
+        private static Connection Con = null;
+
+        private Connection()
+        {
+            this.Base = "MAD_HOTEL";
+            this.Servidor = "DESKTOP-DBRQT41\\SQLEXPRESS";
+            this.Usuario = "daren";
+            this.Clave = "1234";
+            this.Seguridad = true;
+        }
+
+        public static Connection Get_Instancia()
+        {
+            if (Con == null)
+            {
+                Con = new Connection();
+            }
+            return Con;
+        }
+
+        public SqlConnection CrearConexion()
+        {
+            SqlConnection Cadena = new SqlConnection();
+            try
+            {
+                Cadena.ConnectionString = "Server=" + this.Servidor + ";Database=" + this.Base + ";";
+                if (this.Seguridad)
+                {
+                    Cadena.ConnectionString = Cadena.ConnectionString + "Integrated Security = SSPI";
+                }
+
+                else
+                {
+                    Cadena.ConnectionString = Cadena.ConnectionString + "User Id=" + this.Usuario + ";Password=" + this.Clave;
+                }
+
+            }
+            catch (Exception ex)
+            {
+                Cadena = null;
+                throw ex;
+            }
+            return Cadena;
+        }
+    }
+}
---
+++ Sistema/Sistema/D_Usuario.cs
@@ -0,0 +1,108 @@
+﻿using Sistema.Entidad;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sistema.Datos
+{
+    public class D_Usuario
+    {
+        public string sp_Usuario(Usuario Obj)
+        {
+            string Rpta = "";
+            SqlConnection SqlCon = new SqlConnection();
+            try
+            {
+                SqlCon = Connection.Get_Instancia().CrearConexion();
+                SqlCommand Comando = new SqlCommand("sp_Usuario", SqlCon);
+                Comando.CommandType = CommandType.StoredProcedure;
+                Comando.Parameters.Add("@RFC", SqlDbType.VarChar).Value = Obj.RFC;
+                Comando.Parameters.Add("@nombre", SqlDbType.VarChar).Value = Obj.nombre;
+                Comando.Parameters.Add("@apellidop", SqlDbType.VarChar).Value = Obj.apellidop;
+                Comando.Parameters.Add("@apellidom", SqlDbType.VarChar).Value = Obj.apellidom;
+                Comando.Parameters.Add("@nomina", SqlDbType.Float).Value = Obj.nomina;
+                Comando.Parameters.Add("@fnacimiento", SqlDbType.Date).Value = Obj.fnacimiento;
+                Comando.Parameters.Add("@domicilio", SqlDbType.VarChar).Value = Obj.domicilio;
+                Comando.Parameters.Add("@ciudad", SqlDbType.VarChar).Value = Obj.ciudad;
+                Comando.Parameters.Add("@estado", SqlDbType.VarChar).Value = Obj.estado;
+                Comando.Parameters.Add("@pais", SqlDbType.VarChar).Value = Obj.pais;
+                Comando.Parameters.Add("@telefono", SqlDbType.Float).Value = Obj.telefono;
+                Comando.Parameters.Add("@esadmin", SqlDbType.Int).Value = Obj.esadmin;
+                Comando.Parameters.Add("@correo", SqlDbType.VarChar).Value = Obj.correo;
+                Comando.Parameters.Add("@contrasena", SqlDbType.VarChar).Value = Obj.contrasena;
+                Comando.Parameters.Add("@Op", SqlDbType.Char).Value = Obj.Op;
+                SqlCon.Open();
+                Rpta = Comando.ExecuteNonQuery() == 1 ? "OK" : "OK";
+            }
+            catch (Exception ex)
+            {
+                Rpta = ex.Message;
+            }
+            finally
+            {
+                if (SqlCon.State == ConnectionState.Open) SqlCon.Close();
+            }
+
+
+            return Rpta;
+        }
+
+        public DataTable sp_Login_Usuario(string correo)
+        {
+            SqlDataReader Resultado;
+            DataTable Tabla = new DataTable();
+            SqlConnection SqlCon = new SqlConnection();
+            try
+            {
+                SqlCon = Connection.Get_Instancia().CrearConexion();
+                SqlCommand Comando = new SqlCommand("sp_Login_Usuario", SqlCon);
+                Comando.CommandType = CommandType.StoredProcedure;
+                Comando.Parameters.Add("@correo", SqlDbType.VarChar).Value = correo;
+                SqlCon.Open();
+                Resultado = Comando.ExecuteReader();
+                Tabla.Load(Resultado);
+                return Tabla;
+            }
+            catch (Exception ex)
+            {
+                return null;
+                throw ex;
+            }
+            finally
+            {
+                if (SqlCon.State == ConnectionState.Open) SqlCon.Close();
+            }
+        }
+
+        public DataTable sp_Get_Usuario()
+        {
+            SqlDataReader Resultado;
+            DataTable Table = new DataTable();
+            SqlConnection SqlCon = new SqlConnection();
+
+            try
+            {
+                SqlCon = Connection.Get_Instancia().CrearConexion();
+                SqlCommand Comando = new SqlCommand("sp_Get_Usuario", SqlCon);
+                Comando.CommandType = CommandType.StoredProcedure;
+                SqlCon.Open();
+                Resultado = Comando.ExecuteReader();
+                Table.Load(Resultado);
+                return Table;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                if (SqlCon.State == ConnectionState.Open) SqlCon.Close();
+            }
+        }
+
+    }
+}
