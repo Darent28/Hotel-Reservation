9a59ea3d679ab9de3719763407f2153bcc8355ec
David Elizondo <61263018+Darent28@users.noreply.github.com>
2023-04-13 09:38:22 -0600

    Add files via upload

--- Querys/Sp.sql
+++ Querys/Sp.sql
@@ -13,9 +13,6 @@ create proc sp_Usuario
 @nomina                        numeric(5,0),
 @fnacimiento				   DATE,
 @domicilio					   varchar(255),
-@ciudad						   varchar(255),
-@estado						   varchar(255),
-@pais						   varchar(255),
 @telefono					   numeric(10,0),
 @esadmin					   int,
 @correo						   varchar(255),
@@ -25,14 +22,14 @@ as
 Begin
 	IF @Op = 'I' --insert
 	Begin
-		insert into Usuario (RFC, nombre, apellidop, apellidom, nomina, fnacimiento, domicilio, ciudad, estado, pais, telefono, esadmin, correo, contrasena)
-				       	Values (@RFC, @nombre, @apellidop, @apellidom, @nomina, @fnacimiento, @domicilio, @ciudad, @estado, @pais, @telefono, @esadmin, @correo, @contrasena)
+		insert into Usuario (RFC, nombre, apellidop, apellidom, nomina, fnacimiento, domicilio, telefono, esadmin, correo, contrasena)
+				       	Values (@RFC, @nombre, @apellidop, @apellidom, @nomina, @fnacimiento, @domicilio, @telefono, @esadmin, @correo, @contrasena)
 	End
 
 	IF @Op = 'U' --update
 	Begin
 		Update Usuario set nombre = @nombre, apellidop = @apellidop, apellidom = @apellidom, nomina = @nomina,
-						   fnacimiento = @fnacimiento, ciudad = @ciudad,  estado = @estado, pais = @pais, telefono = @telefono,
+						   fnacimiento = @fnacimiento, telefono = @telefono,
 						   esadmin = @esadmin, correo = @correo, contrasena = @contrasena
 		where RFC = @RFC	
 	End
@@ -52,7 +49,7 @@ go
 create proc sp_Login_Usuario
 @Correo varchar (50)
 as
-		select contrasena, RFC from  Usuario
+		select contrasena, RFC, concat(nombre, ' ', apellidop, ' ', apellidom) as nombreCom, esadmin from  Usuario
 		where  correo=@correo
 go
 
@@ -62,5 +59,69 @@ go
 
 create proc sp_Get_Usuario
 as
-	select * from  Usuario;
+	select Case esadmin 
+	when 0 then 'Operador' 
+	when 1 then 'Administrador' 
+	ELSE 'Desconocido' END AS Puesto, 
+	RFC, nombre as Nombre, apellidop as 'Apellido Paterno',  apellidom as 'Apellido Materno',
+	nomina as Nomina, fnacimiento as 'Fecha de nacimiento', domicilio as Domicilio, telefono as 'Telefono celular', correo as Correo, contrasena as Contaseña 
+	     from  Usuario;
+go
+
+--------------------------------------------------------------------------Hotel--------------------------------------------------------------------------------------------------------------------
+
+If OBJECT_ID ('sp_Hotel') is not null
+	Drop procedure sp_Hotel;
+go
+
+
+create proc sp_Hotel
+(
+@Op							   CHAR(1),
+@id_hotel					   int,
+@nombreH					   varchar(255),
+@ubicacion					   varchar(255),
+@domicilioH					   varchar(255),
+@numPiso					   numeric(3,0),
+@canHabitacion				   numeric(2,0),
+@zonaTuris					   Bit,
+@servicioAdi				   Bit,
+@frentePlaya				   Bit,
+@salonEventos				   Bit,
+@fechaReg					   DATE, 
+@fechaInicioOp				   DATE,
+@regAdim					   varchar(13)
+)
+as
+Begin
+	IF @Op = 'I' --insert
+	Begin
+		insert into Hotel (nombreH, ubicacion, domicilioH, numPiso, canHabitacion, zonaTuris, servicioAdi, frentePlaya, salonEventos, fechaReg, fechaInicioOp, regAdim)
+				       	Values (@nombreH, @ubicacion, @domicilioH, @numPiso, @canHabitacion, @zonaTuris, @servicioAdi, @frentePlaya, @salonEventos, @fechaReg, @fechaInicioOp, @regAdim)
+	End
+
+	IF @Op = 'U' --update
+	Begin
+		Update Hotel set nombreH = @nombreH, ubicacion = @ubicacion, domicilioH = @domicilioH, numPiso = @numPiso,
+						   canHabitacion = @canHabitacion, zonaTuris = @zonaTuris,
+						   servicioAdi = @servicioAdi, frentePlaya = @frentePlaya, salonEventos = @salonEventos
+		where id_hotel = @id_hotel	
+	End
+
+	IF @Op = 'D' --delete
+	Begin
+		delete from Hotel  where id_hotel = @id_hotel	
+	End
+
+End;
+go
+
+
+If OBJECT_ID ('sp_Get_Hotel') is not null
+	Drop procedure sp_Get_Hotel;
+go
+
+create proc sp_Get_Hotel
+as
+	select * from  Hotel;
 go
--- Querys/Tablas.sql
+++ Querys/Tablas.sql
@@ -9,9 +9,6 @@ create table Usuario (
 	nomina numeric(5,0) not null,
 	fnacimiento DATE not null,
 	domicilio varchar(255) not null,
-	ciudad varchar(255) not null,
-	estado varchar(255) not null,
-	pais varchar(255) not null,
 	telefono numeric(10,0) not null,
 	esadmin int not null,
 	correo varchar(255) not null,
@@ -20,7 +17,139 @@ create table Usuario (
 
 drop table Usuario;
 
+alter table usuario drop column ciudad, estado, pais;
+
 select * from Usuario;
 
 truncate table Usuario;
 
+update Usuario set correo = 'operador@mail.com', contrasena = 'aei', nombre = 'Operador', apellidop = 'Primer', apellidom = 'Prueba' where RFC = 'EIRD012807MDA';
+
+------------------------------------------------------------------- Cliente-------------------------------------------------------------------------------------------------
+
+create table Cliente(
+	RFC varchar(13) primary key,
+	nombre varchar(255) not null,
+	apellidoP varchar(255) not null,
+	apellidoM varchar(255) not null,
+	domicilio varchar(255) not null,
+	correo varchar(255) not null,
+	telefono numeric(10,0) not null,
+	referencia varchar(255) not null,
+	fNacimiento DATE,
+	eCivil varchar(255) not null,
+	fRegistro DATE,
+	regAdim varchar(13) not null,
+	foreign key (regAdim) REFERENCES Usuario(RFC)
+);
+
+------------------------------------------------------------------- Hotel-------------------------------------------------------------------------------------------------
+
+create table Hotel(
+	id_hotel int primary key identity,
+	nombreH  varchar(255) not null,
+	ubicacion varchar(255) not null,
+	domicilioH varchar(255) not null,
+	numPiso numeric(3,0) not null,
+	canHabitacion numeric(2,0) not null,
+	zonaTuris Bit DEFAULT 0,
+	servicioAdi Bit DEFAULT 0,
+	frentePlaya Bit DEFAULT 0,
+	salonEventos Bit DEFAULT 0,
+	fechaReg DATE, 
+	fechaInicioOp DATE,
+	regAdim varchar(13) not null,
+	foreign key (regAdim) REFERENCES Usuario(RFC)
+);
+
+drop table Hotel
+select * from Hotel
+alter table Hotel drop column servicioAdi;
+
+------------------------------------------------------------------- Habitación-------------------------------------------------------------------------------------------------
+
+create table Habitacion(
+	id_habitacion int primary key identity,
+	numCamas numeric(1,0) not null,
+	tipoCama varchar(255) not null,	
+	preXpersXnoc numeric(5,2) not null, --precio de persona por noche
+	canXpersXhab numeric(1,0) not null, --cantidad de persona por habitacion
+	nivelHab varchar(255) not null,	
+	frente int not null,
+	rfcCliente varchar(13) not null,
+	id_hotel int not null,
+	foreign key (id_hotel) REFERENCES Hotel(id_hotel),
+	foreign key (rfcCliente) REFERENCES Cliente(RFC)
+);
+ 
+ALTER TABLE Habitacion
+DROP COLUMN precio; 
+drop table Habitacion;
+------------------------------------------------------------------- Reservacion-------------------------------------------------------------------------------------------------
+
+create table Reservacion(
+	codigo varchar(9) primary key not null,
+	fReser DATE,
+	fInicio DATE,
+	fFin DATE,
+	nombreC varchar(255) not null,	
+	nombreH varchar(255) not null,	
+	habitacion varchar(255) not null,	
+	canPers numeric(1,0) not null,
+	anticipo numeric(5,2) not null,
+	mPago varchar(255) not null,
+	regAdim varchar(13) not null,
+	rfcCliente varchar(13) not null,
+	id_hotel int not null,
+	foreign key (regAdim) REFERENCES Usuario(RFC),
+	foreign key (rfcCliente) REFERENCES Cliente(RFC),
+	foreign key (id_hotel) REFERENCES Hotel(id_hotel)
+
+); 
+
+ALTER TABLE Reservacion
+DROP COLUMN descuento; 
+
+drop table Reservacion;
+------------------------------------------------------------------- Check in-------------------------------------------------------------------------------------------------
+
+create table Checkin(
+	id_checkin int primary key identity,
+	asistio Bit DEFAULT 0,
+	RFC_Cliente varchar(13),
+	foreign key (RFC_Cliente) REFERENCES Cliente(RFC)
+
+);
+
+drop table Checkin
+
+------------------------------------------------------------------- Check out-------------------------------------------------------------------------------------------------
+
+create table Checkout(
+	id_checkout int primary key identity,
+	extendio Bit DEFAULT 0,
+	id_checkin int, 
+	foreign key (id_checkin) REFERENCES Checkin(id_checkin)
+);
+
+drop table Checkout
+------------------------------------------------------------------- Factura-------------------------------------------------------------------------------------------------
+
+create table Factura(
+	numFactura numeric(5,0) primary key,
+	id_hotel int not null,
+	id_cliente varchar(13)  not null,
+	cod_reser varchar(9) not null,
+	servUsado varchar(255) not null, 
+	precio numeric(5,2) not null,
+	descuento numeric(5,2) not null,
+	montoTotal numeric(5,2) not null,
+	id_checkout int,
+	foreign key (id_hotel) REFERENCES Hotel(id_hotel),
+	foreign key (id_cliente) REFERENCES Cliente(RFC),
+	foreign key (cod_reser) REFERENCES Reservacion(codigo),
+	foreign key (id_checkout) REFERENCES Checkout(id_checkout)
+
+);
+
+drop table Factura;
