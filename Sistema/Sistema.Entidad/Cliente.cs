513d1091b620d15566a5b3abed470a1cf471372d
David Elizondo <61263018+Darent28@users.noreply.github.com>
2023-04-24 11:32:36 -0600

    Add files via upload

---
+++ Sistema/Sistema.Entidad/Cliente.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sistema.Entidad
+{
+    public class Cliente
+    {
+        public string RFC { get; set; }
+        public string nombre { get; set; }
+        public string apellidoP { get; set; }
+        public string apellidoM { get; set; }
+        public string domicilio { get; set; }
+        public string correo { get; set; }
+        public double telefono { get; set; }
+        public string referencia { get; set; }
+        public string fNacimiento { get; set; }
+        public string eCivil { get; set; }
+        public string fRegistro { get; set; }
+        public string regAdim { get; set; }
+        public string Op { get; set; }
+
+    }
+}
---
+++ Sistema/Sistema.Entidad/Habitacion.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sistema.Entidad
+{
+    public class Habitacion
+    {
+        public int id_habitacion { get; set; }
+        public double numCamas { get; set; }
+        public string tipoCama { get; set; }
+        public double preXpersXnoc { get; set; }
+        public double canXpersXhab { get; set; }
+        public string nivelHab { get; set; }
+        public int frente { get; set; }
+        public string regAdim { get; set; }
+        public string Op { get; set; }
+    }
+}
