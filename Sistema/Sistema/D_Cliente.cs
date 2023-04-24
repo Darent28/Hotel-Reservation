44b0ee1adeda784e4802af8b57b618931fcdd288
David Elizondo <61263018+Darent28@users.noreply.github.com>
2023-04-24 11:34:35 -0600

    Add files via upload

---
+++ Sistema/Sistema/D_Cliente.cs
@@ -0,0 +1,79 @@
+﻿using Sistema.Entidad;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sistema.Datos
+{
+    public class D_Cliente
+    {
+        public string sp_Cliente(Cliente Obj)
+        {
+            string Rpta = "";
+            SqlConnection SqlCon = new SqlConnection();
+            try
+            {
+                SqlCon = Connection.Get_Instancia().CrearConexion();
+                SqlCommand Comando = new SqlCommand("sp_Cliente", SqlCon);
+                Comando.CommandType = CommandType.StoredProcedure;
+                Comando.Parameters.Add("@RFC", SqlDbType.VarChar).Value = Obj.RFC;
+                Comando.Parameters.Add("@nombre", SqlDbType.VarChar).Value = Obj.nombre;
+                Comando.Parameters.Add("@apellidoP", SqlDbType.VarChar).Value = Obj.apellidoP;
+                Comando.Parameters.Add("@apellidoM", SqlDbType.VarChar).Value = Obj.apellidoM;
+                Comando.Parameters.Add("@domicilio", SqlDbType.VarChar).Value = Obj.domicilio;
+                Comando.Parameters.Add("@correo", SqlDbType.VarChar).Value = Obj.correo;
+                Comando.Parameters.Add("@telefono", SqlDbType.Float).Value = Obj.telefono;
+                Comando.Parameters.Add("@referencia", SqlDbType.VarChar).Value = Obj.referencia;
+                Comando.Parameters.Add("@fNacimiento", SqlDbType.Date).Value = Obj.fNacimiento;
+                Comando.Parameters.Add("@eCivil", SqlDbType.VarChar).Value = Obj.eCivil;
+                Comando.Parameters.Add("@fRegistro", SqlDbType.Date).Value = Obj.fRegistro;
+                Comando.Parameters.Add("@regAdim", SqlDbType.VarChar).Value = Obj.regAdim;
+                Comando.Parameters.Add("@Op", SqlDbType.Char).Value = Obj.Op;
+                SqlCon.Open();
+                Rpta = Comando.ExecuteNonQuery() == 1 ? "OK" : "OK";
+            }
+            catch (Exception ex)
+            {
+                Rpta = ex.Message;
+            }
+            finally
+            {
+                if (SqlCon.State == ConnectionState.Open) SqlCon.Close();
+            }
+
+
+            return Rpta;
+        }
+
+        public DataTable sp_Get_Cliente()
+        {
+            SqlDataReader Resultado;
+            DataTable Table = new DataTable();
+            SqlConnection SqlCon = new SqlConnection();
+
+            try
+            {
+                SqlCon = Connection.Get_Instancia().CrearConexion();
+                SqlCommand Comando = new SqlCommand("sp_Get_Cliente", SqlCon);
+                Comando.CommandType = CommandType.StoredProcedure;
+                SqlCon.Open();
+                Resultado = Comando.ExecuteReader();
+                Table.Load(Resultado);
+                return Table;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                if (SqlCon.State == ConnectionState.Open) SqlCon.Close();
+            }
+        }
+
+    }
+}
---
+++ Sistema/Sistema/D_Habitacion.cs
@@ -0,0 +1,74 @@
+﻿using Sistema.Entidad;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sistema.Datos
+{
+    public class D_Habitacion
+    {
+        public string sp_Habitacion(Habitacion Obj)
+        {
+            string Rpta = "";
+            SqlConnection SqlCon = new SqlConnection();
+            try
+            {
+                SqlCon = Connection.Get_Instancia().CrearConexion();
+                SqlCommand Comando = new SqlCommand("sp_Habitacion", SqlCon);
+                Comando.CommandType = CommandType.StoredProcedure;
+                Comando.Parameters.Add("@id_habitacion", SqlDbType.Int).Value = Obj.id_habitacion;
+                Comando.Parameters.Add("@numCamas", SqlDbType.Float).Value = Obj.numCamas;
+                Comando.Parameters.Add("@tipoCama", SqlDbType.VarChar).Value = Obj.tipoCama;
+                Comando.Parameters.Add("@preXpersXnoc", SqlDbType.Float).Value = Obj.preXpersXnoc;
+                Comando.Parameters.Add("@canXpersXhab", SqlDbType.Float).Value = Obj.canXpersXhab;
+                Comando.Parameters.Add("@nivelHab", SqlDbType.VarChar).Value = Obj.nivelHab;
+                Comando.Parameters.Add("@frente", SqlDbType.Int).Value = Obj.frente;
+                Comando.Parameters.Add("@regAdim", SqlDbType.VarChar).Value = Obj.regAdim;
+                Comando.Parameters.Add("@Op", SqlDbType.Char).Value = Obj.Op;
+                SqlCon.Open();
+                Rpta = Comando.ExecuteNonQuery() == 1 ? "OK" : "OK";
+            }
+            catch (Exception ex)
+            {
+                Rpta = ex.Message;
+            }
+            finally
+            {
+                if (SqlCon.State == ConnectionState.Open) SqlCon.Close();
+            }
+
+
+            return Rpta;
+        }
+
+        public DataTable sp_Get_Habitacion()
+        {
+            SqlDataReader Resultado;
+            DataTable Table = new DataTable();
+            SqlConnection SqlCon = new SqlConnection();
+
+            try
+            {
+                SqlCon = Connection.Get_Instancia().CrearConexion();
+                SqlCommand Comando = new SqlCommand("sp_Get_Habitacion", SqlCon);
+                Comando.CommandType = CommandType.StoredProcedure;
+                SqlCon.Open();
+                Resultado = Comando.ExecuteReader();
+                Table.Load(Resultado);
+                return Table;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                if (SqlCon.State == ConnectionState.Open) SqlCon.Close();
+            }
+        }
+    }
+}
