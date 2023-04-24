e87b07c0fe846ffbb4204a8b4128ca1b03504401
David Elizondo <61263018+Darent28@users.noreply.github.com>
2023-04-24 11:33:04 -0600

    Add files via upload

---
+++ Sistema/Sistema.Negocio/N_Cliente.cs
@@ -0,0 +1,43 @@
+﻿using Sistema.Datos;
+using Sistema.Entidad;
+using System;
+using System.Data;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sistema.Negocio
+{
+    public class N_Cliente
+    {
+        public static string sp_Cliente(string RFC, string nombre, string apellidoP, string apellidoM, 
+                                        string domicilio, string correo, double telefono, string referencia,
+                                        string fNacimiento, string eCivil, string fRegistro, string regAdim, string Op)
+        {
+            D_Cliente cliente = new D_Cliente();
+            Cliente Obj = new Cliente();
+            Obj.RFC = RFC;
+            Obj.nombre = nombre;
+            Obj.apellidoP = apellidoP;
+            Obj.apellidoM = apellidoM;
+            Obj.domicilio = domicilio;
+            Obj.correo = correo;
+            Obj.telefono = telefono;
+            Obj.referencia = referencia;
+            Obj.fNacimiento = fNacimiento;
+            Obj.eCivil = eCivil;
+            Obj.fRegistro = fRegistro;
+            Obj.regAdim = regAdim;
+            Obj.Op = Op;
+
+            return cliente.sp_Cliente(Obj);
+        }
+
+        public static DataTable sp_Get_Cliente()
+        {
+            D_Cliente cliente = new D_Cliente();
+            return cliente.sp_Get_Cliente();
+        }
+    }
+}
---
+++ Sistema/Sistema.Negocio/N_Habitacion.cs
@@ -0,0 +1,55 @@
+﻿using Sistema.Datos;
+using Sistema.Entidad;
+using System;
+using System.Data;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sistema.Negocio
+{
+    public class N_Habitacion
+    {
+        public static string sp_Habitacion(double numCamas, string tipoCama, double preXpersXnoc,
+                                       double canXpersXhab, string nivelHab, int frente, string regAdim, string Op)
+        {
+            D_Habitacion habitacion = new D_Habitacion();
+            Habitacion Obj = new Habitacion();
+            Obj.numCamas = numCamas;
+            Obj.tipoCama = tipoCama;
+            Obj.preXpersXnoc = preXpersXnoc;
+            Obj.canXpersXhab = canXpersXhab;
+            Obj.nivelHab = nivelHab;
+            Obj.frente = frente;;
+            Obj.regAdim = regAdim;
+            Obj.Op = Op;
+
+            return habitacion.sp_Habitacion(Obj);
+        }
+
+        public static string sp_Habitacion_UD(int id_habitacion, double numCamas, string tipoCama, double preXpersXnoc,
+                                      double canXpersXhab, string nivelHab, int frente, string regAdim, string Op)
+        {
+            D_Habitacion habitacion = new D_Habitacion();
+            Habitacion Obj = new Habitacion();
+            Obj.id_habitacion = id_habitacion;
+            Obj.numCamas = numCamas;
+            Obj.tipoCama = tipoCama;
+            Obj.preXpersXnoc = preXpersXnoc;
+            Obj.canXpersXhab = canXpersXhab;
+            Obj.nivelHab = nivelHab;
+            Obj.frente = frente; ;
+            Obj.regAdim = regAdim;
+            Obj.Op = Op;
+
+            return habitacion.sp_Habitacion(Obj);
+        }
+
+        public static DataTable sp_Get_Habitacion()
+        {
+            D_Habitacion habitacion = new D_Habitacion();
+            return habitacion.sp_Get_Habitacion();
+        }
+    }
+}
